be05c17a70cd9a92191860871623386033da9e18
Xpresh <ivax132001@gmail.com>
2025-03-25 13:14:58 +0100

    Estructura inicial creada, index i rutes i controladors usuaris (per acabar)

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+// Fitxer principal que arrenca el servidor
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+//const userRoutes = require('./routes/userRoutes');
+//const designRoutes = require('./routes/designRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//app.use('/api/users', userRoutes);
+//app.use('/api/designs', designRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Servidor funcionant al port ${PORT}`));
