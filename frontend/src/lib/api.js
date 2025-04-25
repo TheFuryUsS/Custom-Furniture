a5d3a36f48d2832c0a1f3d936b38cff002480ddc
Xpresh <ivax132001@gmail.com>
2025-04-25 17:05:10 +0200

    Afegit arxiu per cridar al backend

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,10 @@
+// Arxiu per cridar l'api i tenir-ho unificat
+
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: { 'Content-Type': 'application/json' }
+});
+
+export default api;
