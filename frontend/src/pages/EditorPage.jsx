9b37f882067ef67b6b249dd99e47e05c0dd04baa
Xpresh <ivax132001@gmail.com>
2025-05-09 16:51:36 +0200

    Petit canvi no navegar a dissenys inexistents

--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import api from '../lib/api';
 import EditorHeader from '../components/EditorHeader';
 import CanvasEditor from '../components/CanvasEditor';
 
 export default function EditorPage() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get(`/designs/${id}`)
+            .then(res => {
+                if (!res.data && !res.data.id)
+                    navigate('/designs');
+            })
+            .catch(() => {
+                navigate('/designs');
+            })
+    }, id);
+
     return (
-        <div className="flex">
+        <div className="flex flex-col h-screen">
             <div className="flex-grow p-2">
                 <CanvasEditor />
             </div>
